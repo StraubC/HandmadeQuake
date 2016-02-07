fffec360d2b146b77dedcf8ac3e0a098ead74852
StraubC <christianstraub2609@googlemail.com>
2016-02-07 16:55:26 +0100

    Module 2
    complete project incl. module 2

---
+++ Handmade Quake/quakedef.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+
+typedef uint8_t uint8;
+typedef int32_t int32;
+typedef uint32_t uint32;
+
+
+#include "common.h"
+#include "q_stl.h"
+#include "host.h"
